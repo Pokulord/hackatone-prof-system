40923dca788fd36a08eb7bc631d3a70b57513f27
Merrow <1nf1n1t750@gmail.com>
2025-10-08 18:53:04 +0500

    full

--- src/Endpoints/MonitoringEndpoints.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "../crow_all.h"
-
-#include "../Logger/Logger.h"
-
-void registerMonitoringEndpoints(crow::SimpleApp& app, Logger& logger);
--- src/Endpoints/UserEndpoints.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "crow_all.h"
-#include "../UseCases/UserService.h"
-#include "../jwt_utils.h"
-
-#include "../Logger/Logger.h"
-
-void registerUserEndpoints(crow::SimpleApp& app, UserService& userService, JwtUtils& jwtUtils, Logger& logger);
